d1b31ac3ebfd8bd319cc7cc6479717a91bd82493
Vladimir Psyukalov <auremsinistram@gmail.com>
2022-07-16 22:18:13 +0300

    Commit - 3

--- Sources/Extensions/Common/Array+Common.swift
+++ Sources/Extensions/Common/Array+Common.swift
@@ -11,4 +11,11 @@ public extension Array {
     subscript(safe index: Int) -> Element? {
         return 0 <= index && index < count ? self[index] : nil
     }
+    
+    // MARK: - Public mutating func
+    
+    mutating func move(at atIndex: Int, to toIndex: Int) {
+        let element = remove(at: atIndex)
+        insert(element, at: toIndex)
+    }
 }
